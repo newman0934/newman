eb57673ad1c760247c716bdf08facc93046d2b61
caesar <caesarwang0934@gmail.com>
2022-02-17 16:32:22 +0800

    order modal 調整

--- src/components/OrderModal.vue
+++ src/components/OrderModal.vue
@@ -1,170 +1,117 @@
 <template>
-<teleport to='#model'>
-   <div
-    class="modal fade hidden"
-    id="productModal"
-    tabindex="-1"
-    role="dialog"
-    aria-labelledby="exampleModalLabel"
-    aria-hidden="true"
-    ref="modal"
-  >
-    <div class="modal-dialog modal-xl" role="document">
-      <div class="modal-content border-0">
-        <div class="modal-header bg-dark text-white">
-          <h5 class="modal-title" id="exampleModalLabel">
-            <span>訂單細節</span>
-          </h5>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
-        </div>
-        <div class="modal-body">
-          <div class="row">
-            <div class="col-md-4">
-              <h3>用戶資料</h3>
-              <table class="table">
-                <tbody v-if="tempOrder.user">
-                  <tr>
-                    <th style="width: 100px">姓名</th>
-                    <td>{{ tempOrder.user.name }}</td>
-                  </tr>
-                  <tr>
-                    <th>Email</th>
-                    <td>{{ tempOrder.user.email }}</td>
-                  </tr>
-                  <tr>
-                    <th>電話</th>
-                    <td>{{ tempOrder.user.tel }}</td>
-                  </tr>
-                  <tr>
-                    <th>地址</th>
-                    <td>{{ tempOrder.user.address }}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <div class="col-md-8">
-              <h3>訂單細節</h3>
-              <table class="table">
-                <tbody>
-                  <tr>
-                    <th style="width: 100px">訂單編號</th>
-                    <td>{{ tempOrder.id }}</td>
-                  </tr>
-                  <tr>
-                    <th>下單時間</th>
-                    <td>{{ $filters.date(tempOrder.create_at) }}</td>
-                  </tr>
-                  <tr>
-                    <th>付款時間</th>
-                    <td>
-                      <span v-if="tempOrder.paid_date">
-                        {{ $filters.date(tempOrder.paid_date) }}
-                      </span>
-                      <span v-else>時間不正確</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th>付款狀態</th>
-                    <td>
-                      <strong v-if="tempOrder.is_paid" class="text-success"
-                        >已付款</strong
-                      >
-                      <span v-else class="text-muted">尚未付款</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th>總金額</th>
-                    <td>
-                      {{ $filters.currency(tempOrder.total) }}
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-              <h3>選購商品</h3>
-              <table class="table">
-                <thead>
-                  <tr></tr>
-                </thead>
-                <tbody>
-                  <tr v-for="item in tempOrder.products" :key="item.id">
-                    <th>
-                      {{ item.product.title }}
-                    </th>
-                    <td>{{ item.qty }} / {{ item.product.unit }}</td>
-                    <td class="text-end">
-                      {{ $filters.currency(item.final_total) }}
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-              <div class="d-flex justify-content-end">
-                <div class="form-check">
-                  <input
-                    class="form-check-input"
-                    type="checkbox"
-                    value=""
-                    id="flexCheckDefault"
-                    v-model="tempOrder.is_paid"
-                  />
-                  <label class="form-check-label" for="flexCheckDefault">
-                    <span v-if="tempOrder.is_paid">已付款</span>
-                    <span v-else>未付款</span>
-                  </label>
-                </div>
-              </div>
-            </div>
+  <teleport to="#model">
+    <div
+      class="bg-modal-bg-opacity flex fixed inset-0 justify-center items-center z-20"
+      :class="{ hidden: !modalToggle }"
+    >
+      <div class="bg-white max-w-3xl w-full p-10 rounded-md">
+        <h5 class="text-center text-3xl mb-5">
+          <span>訂單細節</span>
+        </h5>
+        <div class="flex space-x-10 mb-10">
+          <div>
+            <h3 class="text-2xl mb-5">用戶資料</h3>
+            <table class="table-auto text-left">
+              <tbody v-if="tempOrder.user">
+                <tr>
+                  <th class="pr-5">姓名</th>
+                  <td>{{ tempOrder.user.name }}</td>
+                </tr>
+                <tr>
+                  <th class="pr-5">Email</th>
+                  <td>{{ tempOrder.user.email }}</td>
+                </tr>
+                <tr>
+                  <th class="pr-5">電話</th>
+                  <td>{{ tempOrder.user.tel }}</td>
+                </tr>
+                <tr>
+                  <th class="pr-5">地址</th>
+                  <td>{{ tempOrder.user.address }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div>
+            <h3 class="text-2xl mb-5">訂單細節</h3>
+            <table class="table-auto text-left">
+              <tbody>
+                <tr>
+                  <th class="pr-5">訂單編號</th>
+                  <td>{{ tempOrder.id }}</td>
+                </tr>
+                <tr>
+                  <th class="pr-5">下單時間</th>
+                  <td>{{ $filters.date(tempOrder.create_at) }}</td>
+                </tr>
+                <tr>
+                  <th class="pr-5">付款時間</th>
+                  <td>
+                    <span v-if="tempOrder.paid_date">{{ $filters.date(tempOrder.paid_date) }}</span>
+                    <span v-else>時間不正確</span>
+                  </td>
+                </tr>
+                <tr>
+                  <th class="pr-5">付款狀態</th>
+                  <td>
+                    <strong v-if="tempOrder.is_paid" class="text-success">已付款</strong>
+                    <span v-else class="text-muted">尚未付款</span>
+                  </td>
+                </tr>
+                <tr>
+                  <th class="pr-5">總金額</th>
+                  <td>{{ $filters.currency(tempOrder.total) }}</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
-        <div class="modal-footer">
-          <button
-            type="button"
-            class="btn btn-outline-secondary"
-            data-bs-dismiss="modal"
-          >
-            取消
-          </button>
-          <button
-            type="button"
-            class="btn btn-primary"
-            @click="$emit('update-paid', tempOrder)"
-          >
-            修改付款狀態
-          </button>
-        </div>
-      </div>
-    </div>
-  </div>
-      <div class="bg-modal-bg-opacity fixed hidden inset-0 justify-center items-center z-20 ">
-      <div class="bg-white max-w-3xl w-full p-10 rounded-md">
-                  <h5 class="text-center text-3xl mb-5">
-            <span>訂單細節</span>
+        <div>
+          <h5 class="text-center text-3xl mb-5">
+            <span>購買商品</span>
           </h5>
+          <table class="table-auto w-full mb-10">
+            <tbody>
+              <tr v-for="item in tempOrder.products" :key="item.id" class="text-left">
+                <th>{{ item.product.title }}</th>
+                <td>{{ item.qty }} / {{ item.product.unit }}</td>
+                <td class="text-end">{{ $filters.currency(item.final_total) }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="flex mb-5">
+          <div>
+            <input
+              class="form-check-input"
+              type="checkbox"
+              value
+              id="flexCheckDefault"
+              v-model="tempOrder.is_paid"
+            />
+            <label class="form-check-label" for="flexCheckDefault">
+              <span v-if="tempOrder.is_paid">已付款</span>
+              <span v-else>未付款</span>
+            </label>
+          </div>
+        </div>
         <div class="text-right space-x-5">
           <button
             type="button"
             class="text-primary border-[1px] border-primary rounded-md px-3 py-2 hover:bg-primary hover:text-white"
-          >
-            取消
-          </button>
+            @click="$emit('closeOrderModal')"
+          >取消</button>
           <button
             type="button"
             class="text-red-600 border-[1px] border-red-600 rounded-md px-3 py-2 hover:bg-red-600 hover:text-white"
             @click="$emit('update-paid', tempOrder)"
-          >
-            修改付款狀態
-          </button>
+          >修改付款狀態</button>
         </div>
       </div>
-  </div>
-</teleport>
+    </div>
+  </teleport>
 </template>
 <script>
-import modalMixin from '@/utils/modalMixin.js'
 export default {
   props: {
     order: {
@@ -172,6 +119,10 @@ export default {
       default () {
         return {}
       }
+    },
+    orderModalOpen: {
+      type: Boolean,
+      default: false
     }
   },
   data () {
@@ -179,14 +130,17 @@ export default {
       status: {},
       modal: '',
       tempOrder: {},
-      isPaid: false
+      isPaid: false,
+      modalToggle: false
     }
   },
   watch: {
     order () {
       this.tempOrder = this.order
+    },
+    orderModalOpen () {
+      this.modalToggle = this.orderModalOpen
     }
-  },
-  mixins: [modalMixin]
+  }
 }
 </script>
